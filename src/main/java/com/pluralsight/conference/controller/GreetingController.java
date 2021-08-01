bee803c7d75d760c35b6a5296280b8720a93246b
desh-20 <sandeeshr@techprovint.com>
2021-08-01 20:39:41 +0530

    Page not found bug fixed

--- src/main/java/com/pluralsight/conference/controller/GreetingController.java
+++ src/main/java/com/pluralsight/conference/controller/GreetingController.java
@@ -1,17 +1,21 @@
 package com.pluralsight.conference.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
 @Controller
 public class GreetingController {
 
+
     @GetMapping("greeting")
-    public String greeting(Map<String, Object> model){
-        model.put("message", "Hello Bryan");
+    public String welcome(Map<String, Object> model) {
+        model.put("message","Message is here");
+        System.out.println("model = " + model);
         return "greeting";
     }
 }
--- src/main/webapp/WEB-INF/views/greeting.jsp
+++ src/main/webapp/WEB-INF/views/greeting.jsp
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Greeting</title>
+    <title>greeting</title>
 </head>
 <body>
 <h1>${message}</h1>
