c846b11b8fa20c6494856fabd88df26afe7495a1
desh-20 <sandeeshr@techprovint.com>
2021-08-02 15:10:17 +0530

    Rest controller set up for the default actions

--- src/main/java/com/pluralsight/conference/controller/UserController.java
+++ src/main/java/com/pluralsight/conference/controller/UserController.java
@@ -1,2 +1,22 @@
-package com.pluralsight.conference.controller;public class UserController {
+package com.pluralsight.conference.controller;
+
+import com.pluralsight.conference.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam(value = "firstname", defaultValue = "Bryan")String firstname,
+                        @RequestParam(value = "firstname", defaultValue = "Bryan")String lastname,
+                        @RequestParam(value = "firstname", defaultValue = "Bryan")int age) {
+        User user = new User();
+
+        user.setFirstname(firstname);
+        user.setLastname(lastname);
+        user.setAge(age);
+        return user;
+    }
 }
